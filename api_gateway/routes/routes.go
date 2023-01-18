70d41636f3c54d58b40e5c24dbc65a1e878e8a88
Siddhant Prateek <siddhantprateek@gmail.com>
2023-01-18 12:05:36 +0530

    api gateway routes

---
+++ api_gateway/routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import "github.com/gofiber/fiber/v2"
+
+func AppRoute() {
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Server is running")
+	})
+
+	app.Listen(":4000")
+
+}
