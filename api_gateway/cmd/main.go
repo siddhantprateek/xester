76b3d840f872f17d788983f9c4ee7e7d5ca6c06b
Siddhant Prateek <siddhantprateek@gmail.com>
2023-01-18 12:05:25 +0530

    api gateway entrypoint

---
+++ api_gateway/cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import routes "github.com/siddhantprateek/xester/api_gateway/routes"
+
+func main() {
+	// Running application Server
+	// Listening... to port :4000
+	routes.AppRoute()
+}
