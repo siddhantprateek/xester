b1b103107c0c1e4d81b0025e2105ca04c91b85fb
Siddhant Prateek <siddhantprateek@gmail.com>
2023-01-21 11:14:53 +0530

    chaincode updated

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 
 ### Tech Stack
-![Docker](https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white)
 ![Hyperledger](https://img.shields.io/badge/hyperledger-2F3134?style=for-the-badge&logo=hyperledger&logoColor=white)
 ![Go](https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white)
+![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white)
+![Docker](https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white)
+![Kubernetes](https://img.shields.io/badge/kubernetes-%23326ce5.svg?style=for-the-badge&logo=kubernetes&logoColor=white)
 
 ### Features
 
--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -4,22 +4,35 @@ package chaincode
 
 import (
 	"github.com/hyperledger/fabric-chaincode-go/shim"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"github.com/hyperledger/fabric-contract-api-go/metadata"
 	pb "github.com/hyperledger/fabric-protos-go/peer"
 )
 
 // Fabric enforces that every chaincode must implement Chaincode interface.
 type AppChaincode struct {
+	contractapi.Contract
+	Name               string
+	Info               metadata.InfoMetadata
+	UnknownTransaction interface{}
+	BeforeTransaction  interface{}
+	AfterTransaction   interface{}
 }
 
 // Init: called when the chaincode is instantiated by the blockchain network
 func (c *AppChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-
 	return shim.Success([]byte("Asset modified"))
 }
 
 // Invoke: called when the client invokes a specific function to process
 // the transaction proposal
 func (c *AppChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-
 	return shim.Success([]byte("Invoke"))
+}
+
+// To create the transaction context for the function defined within the smart contract
+// that read and write data to the blockchain ledger.
+func (c *AppChaincode) Create(ctx contractapi.TransactionContextInterface) error {
+
+	return nil
 }
